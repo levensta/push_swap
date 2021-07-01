68015bd7796e35e7a701edfb6962ee57dfed95a5
levensta <levensta@student.21-school.ru>
2021-07-01 22:59:01 +0300

    ready main algorithm (32k operations for 500 elems)

--- main.c
+++ main.c
@@ -12,6 +12,28 @@ int	lst_is_sorted(t_blst *lst)
 	return (1);
 }
 
+void	minimal_rotation(t_blst **list_a, t_blst **list_b, t_blst **list_sort)
+{
+	int		i;
+	t_blst	*finded;
+	t_arg	*element;
+
+	finded = bd_lstfind2(*list_a, (*list_sort)->data, cmp_elements);
+	element = (t_arg *)finded->data;
+	if (element->index > (bd_lstsize(*list_a) / 2))
+	{
+		i = bd_lstsize(*list_a) - element->index;
+		while (i--)
+			implement_instruction("rra", list_a, list_b);
+	}
+	else if (element->index <= (bd_lstsize(*list_a) / 2))
+	{
+		i = element->index;
+		while (i--)
+			implement_instruction("ra", list_a, list_b);
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	t_blst	*list_a;
@@ -20,55 +42,41 @@ int	main(int argc, char **argv)
 
 	initialize(&list_a, &list_sort, argc, &(argv[1]));
 
-	int i;
-	t_blst	*finded;
-	t_arg	*element;
 	while (!lst_is_sorted(list_a))
 	{
 		while (list_a)
 		{
-			if (argc <= 4)
+			if (argc <= 6)
 			{
+				while (argc == 6 && bd_lstsize(list_b) != 2)
+				{
+					if (bd_lstcmp(list_a, bd_lstmin(list_a, cmp_elements), cmp_elements))
+						minimal_rotation(&list_a, &list_b, &list_sort);
+					implement_instruction("pb", &list_a, &list_b);
+					list_sort = list_sort->next;
+				}
 				if (list_a->next && bd_lstcmp(list_a, list_a->next, cmp_elements) > 0)
 				{
 					implement_instruction("sa", &list_a, &list_b);
 					break ;
 				}
-				if (list_a->next && bd_lstcmp(list_a, bd_lstlast(list_a), cmp_elements) > 0)
+				else if (list_a->next && bd_lstcmp(list_a, bd_lstlast(list_a), cmp_elements) > 0)
 				{
 					implement_instruction("rra", &list_a, &list_b);
 					break ;
-				}
-				// if (argc == 6 && list_a->next ...)
-				// {
-				// 	...
-				// }
+				}				
 			}
-			finded = bd_lstfind2(list_a, list_sort->data, cmp_elements);
-			element = (t_arg *)finded->data;
-			if (element->index > (bd_lstsize(list_a) / 2))
+			else
 			{
-				i = bd_lstsize(list_a) - element->index;
-				while (i--)
-					implement_instruction("rra", &list_a, &list_b);
+				minimal_rotation(&list_a, &list_b, &list_sort);
+				if (!list_b && lst_is_sorted(list_a))
+					break;
+				implement_instruction("pb", &list_a, &list_b);
+				list_sort = list_sort->next;
 			}
-			else if (element->index <= (bd_lstsize(list_a) / 2))
-			{
-				i = element->index;
-				while (i--)
-					implement_instruction("ra", &list_a, &list_b);
-			}
-			implement_instruction("pb", &list_a, &list_b);
-			list_sort = list_sort->next;
 		}
 		while (list_b)
 			implement_instruction("pa", &list_a, &list_b);		
 	}
-
-	// printf("-----------------\nlist_a\t size: %d\n", bd_lstsize(list_a));
-	// bd_lstprint(list_a, print_element);
-	// printf("-----------------\nlist_b\t size: %d\n", bd_lstsize(list_b));
-	// bd_lstprint(list_b, print_element);
-	// printf("-----------------\n");
 	exit(0);
 }
